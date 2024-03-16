03ec54d1c647beeb6a0a26f9b3c237f1d0baa718
Sami khan <oneplayer605@gmail.com>
2024-03-16 22:11:13 +0500

    practice in javascript

---
+++ Practice(JS)/JavaScript_Introduction/arrays.js
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4,5]
+
+console.log(arr)
+console.log(arr.length)
+console.log(arr[3])
+
+// arrays can be changed but strings cant be (immutable)
+
+console.log(typeof(arr))
+// arr has type object
+
--- Practice(JS)/JavaScript_Introduction/strings.js
+++ Practice(JS)/JavaScript_Introduction/strings.js
@@ -2,4 +2,6 @@ let a="Sami";
 console.log(a[1]);
 console.log(a.length)
 
-  
+console.log(a.toLowerCase());
+
+console.log(a.slice(0,3))
